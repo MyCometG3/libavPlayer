fcda85a089c650e2d8ea77293d289b671a182d19
MyCometG3 <mochi@da2.so-net.ne.jp>
2011-08-06 11:22:47 +0900

    s/SimplePlayer/libavPlayer/

--- libavPlayer/LAVPStream.m
+++ libavPlayer/LAVPStream.m
@@ -13,7 +13,7 @@
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
  
- SimplePlayer is distributed in the hope that it will be useful,
+ livavPlayer is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
